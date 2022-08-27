6b0918e3838f02acfd84fa71feeec5bfc4842147
chdkj <112231803+chdkj@users.noreply.github.com>
2022-08-27 22:08:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ const CONFIG = {
   APP_SECRET: "fd24f394a0120035f7bb48e187e40c28",
 
   // 微信公众号的模板ID
-  TEMPLATE_ID: "NrZpPGCkMqKrDf5nphVKh2uiRl5-4j8_ymL-5--T5-8",
+  TEMPLATE_ID: "Pme-rZBPm9j2bTxLretTpz_vQLcHmx3_Tt_Y9MtWM5k",
 
   // 天行API的key
   TXApiKey: "a4c35e25dec34e04fcecdbd3866d00b9",
