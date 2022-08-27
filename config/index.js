8029d89c0bd53f6a900e43ea066e59d36e3eb3d9
chdkj <112231803+chdkj@users.noreply.github.com>
2022-08-27 20:50:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,43 +1,43 @@
 const CONFIG = {
   // ================================基础配置 - 无需修改================================
   // 和风天气API
-  HEFENG_API_KEY: "",
+  HEFENG_API_KEY: "9a2b8f1fa4eb4a47870755d6dafc33e5",
 
   // ================================高级配置 - 需要修改================================
 
   // 微信公众号的appID
-  APP_ID: "",
+  APP_ID: "wx7bd53aa41dc46b3b",
 
   // 微信公众号的appsecret
-  APP_SECRET: "",
+  APP_SECRET: "fd24f394a0120035f7bb48e187e40c28",
 
   // 微信公众号的模板ID
-  TEMPLATE_ID: "",
+  TEMPLATE_ID: "khdHVxYx_3VX04DxnE81dLP-nSD59JQmB1v3zTgBY30	",
 
   // 天行API的key
-  TXApiKey: "",
+  TXApiKey: "a4c35e25dec34e04fcecdbd3866d00b9",
 
   // 用户列表 可配置多个用户
   user: [
     {
       // 男(女)朋友的名字（或昵称或网名）
-      userName: "小沐沐吖",
+      userName: "芃芃","楠"
       // 用户列表的`微信号`
-      userId: "",
+      userId: "o8v5Q5_vdjuv8qBAG-vV9sfPppRs", "o8v5Q56Lr95xZA0p6ZVc5RCqtJIg"
       // 星座
-      star: "摩羯座",
+      star: "狮子座","金牛座"
       // 生日 - 格式：08-22
-      birthday: "01-28",
+      birthday: "08-01","05-18"
       // 城市 - 格式：支持省市县区
-      city: "夏邑县",
+      city: "金昌市","金昌市"
     },
   ],
 
   // 计划旅游日 - 格式：2022-05-20
-  tourism: "2022-12-12",
+  tourism: "2026-08-01",
 
   // 恋爱纪念日 - 格式：2022-05-20
-  loveDay: "2022-05-20",
+  loveDay: "2021-08-01",
 
   // ================================功能配置 - 可选修改================================
 
